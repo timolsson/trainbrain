2cd2182f2f52d558e21ce2fdfd68b5251f893090
Tim Olsson <tim.olsson@hyperisland.se>
2020-02-05 12:38:41 +0100

    Final map
    this is the final map with icon and railway

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react"
-import ReactMapGL, {Marker} from "react-map-gl"
-import TopBox from "./components/topbox"
+import React, { useEffect, useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "./App.css";
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -9,20 +9,20 @@ function App() {
     width: "100vw",
     height: "100vh",
     zoom: 8
-  })
-  const [delay, setDelay] = useState("")
+  });
+  const [delay, setDelay] = useState("");
   function fetchData() {
     fetch(
       "https://cors-anywhere.herokuapp.com/https://labs.thetrainbrain.com/knockon/?minutes=30"
     )
       .then(response => response.json())
-      .then(data => setDelay(data))
+      .then(data => setDelay(data));
   }
   useEffect(() => {
     setInterval(() => {
-      fetchData()
-    }, 5000)
-  }, [])
+      fetchData();
+    }, 5000);
+  }, []);
   return (
     <div>
       <ReactMapGL
@@ -31,7 +31,7 @@ function App() {
         mapStyle="mapbox://styles/timolsson/ck5za7t0m0q7r1ipccguemt6i"
         mapboxApiAccessToken="pk.eyJ1IjoidGltb2xzc29uIiwiYSI6ImNrNXo4bmdvODBya3UzbG5qd3hsZXY2YWsifQ.Tl62NRb0hKYTZR8maLDVzA"
         onViewportChange={viewport => {
-          setViewport(viewport)
+          setViewport(viewport);
         }}
       >
         {delay == ""
@@ -42,12 +42,29 @@ function App() {
                 latitude={Number(disruptions.station.lat)}
                 longitude={Number(disruptions.station.lng)}
               >
-                <div>DELAY</div>
+                <img src="train.svg" alt="Train" />
               </Marker>
             ))}
-        <TopBox />
       </ReactMapGL>
+      {delay == ""
+        ? console.log("loading...")
+        : delay.disruptions.map(disruptions => (
+            <div key={disruptions.station.station}>
+              <div className="rectangle1"></div>
+              <div className="rectangle2"></div>
+              <div className="title">The Train Brain</div>
+              <div className="delay-title"></div>
+              <div className="göteborg1">
+                {disruptions.station.station} - {disruptions.delaycount}min
+              </div>
+              <div className="line1"></div>
+              <div className="göteborg2"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+              <div className="line4"></div>
+            </div>
+          ))}
     </div>
-  )
+  );
 }
-export default App
+export default App;
---
+++ src/components/InfoBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InfoBox() {
+  return (
+    <div className="InfoBox">
+      <h1>This is a title</h1>
+      <p>Hyper Island student project for 'The Train Brain'</p>
+    </div>
+  );
+}
+export default InfoBox;
